02d83b4a1484916124e3caa28c637ff687f22c58
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2023-04-01 23:14:19 -0400

    Basic Scaffold added.

---
+++ src/main/java/com/javatechie/springreactivemongocrud/SpringReactiveMongoCrudJavatechieApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.springreactivemongocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveMongoCrudJavatechieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveMongoCrudJavatechieApplication.class, args);
+    }
+
+}
